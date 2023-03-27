d572af05ae7c40d62390fc4c7df78f28f1458266
deanrtaylor1 <deanrtaylor@hotmail.com>
2023-03-27 16:03:01 +0700

    modified server side error class

--- src/errors/server-side-error.ts
+++ src/errors/server-side-error.ts
@@ -3,8 +3,11 @@ import { CustomError } from "./custom-error";
 export class ServerSideError extends CustomError {
   statusCode = 500;
   reason = "Unable to perform operation. Please try again later.";
-  constructor() {
+  constructor(message?: string) {
     super("Unable to perform operation. Please try again later.");
+    if (!!message) {
+      this.reason = message;
+    }
     Object.setPrototypeOf(this, ServerSideError.prototype);
   }
   serializeErrors() {
